e5989db8e18c23cc8eea930e1cedda97279d7236
へーさん <hira9603859504@gmail.com>
2019-06-09 17:59:52 +0900

    update postAC.py

--- postAC.py
+++ postAC.py
@@ -83,6 +83,96 @@ md = open("content/" + filePass + "/" + fileName, "a")
 txt = "\n先日（" + dt.strftime(contestDateTime, '%Y/%m/%d') + "）、" + \
     "AtCoderで開催された[" + contestFullName + \
     "](" + url + ")に参加しました。" + \
-    "\n\n<!--more-->\n\n![レートグラフ](" + imgFileName + ")\n"
+    "\n\n<!--more-->\n\n![レートグラフ](" + imgFileName + ")\n\n\n---\n\n"
 md.writelines(txt)
 md.close()
+
+
+# 問題一覧 スクレイピング
+instance2 = requests.get(url + "/tasks")
+soup2 = BeautifulSoup(instance2.text, 'lxml')
+table = soup2.find("table")
+rows = table.findAll("a")
+links = []
+for a in rows:
+    links.append("https://atcoder.jp" + a.get("href"))
+links = list(set(links))
+links.sort()
+# 各問題 スクレイピング
+for problemURL in links:
+    instance3 = requests.get(problemURL)
+    soup3 = BeautifulSoup(instance3.text, 'lxml')
+    Title = soup3.select_one(
+        "#main-container > div.row > div:nth-child(2) > span")
+    Point = soup3.select_one("#task-statement > span > span.lang-ja > p")
+    TimeRestriction = soup3.select_one(
+        "#main-container > div.row > div:nth-child(2) > p")
+    Q = soup3.select_one(
+        "#task-statement > span > span.lang-ja > div:nth-child(2) > section"
+    )
+    R = soup3.select_one(
+        "#task-statement > span > span.lang-ja > div:nth-child(3) > section"
+    )
+    # テキスト追加
+    md = open("content/" + filePass + "/" + fileName, "a")
+    txt = "> ## " + Title.text + "\n"
+    txt += "> " + Point.text + "\n"
+    txt += "> " + TimeRestriction.text + "\n"
+    txt += ">"
+
+    strQ = str(Q)
+    strQ = strQ.replace('<var>', '$')
+    strQ = strQ.replace('</var>', '$')
+    strQ = strQ.replace('<code>', '`')
+    strQ = strQ.replace('</code>', '`')
+    strQ = strQ.replace('<strong>', '**')
+    strQ = strQ.replace('</strong>', '**')
+    strQ = strQ.replace('_', '\\_')
+    strQ = strQ.replace('<li>', '- ')
+    strQ = strQ.replace('</li>', '')
+    strQ = strQ.replace('<ul>', '\n')
+    strQ = strQ.replace('</ul>', '')
+    strQ = strQ.replace('<ol>', '\n')
+    strQ = strQ.replace('</ol>', '')
+    strQ = strQ.replace('<p>', '\n')
+    strQ = strQ.replace('</p>', '\n')
+    strQ = strQ.replace('<h3>', '### ')
+    strQ = strQ.replace('</h3>', '\n ')
+    strQ = strQ.replace('<section>', '')
+    strQ = strQ.replace('</section>', '')
+    strQ = strQ.replace('\n\n', '\n')
+    strQ = strQ.replace('\n', '\n> ')
+    txt += strQ
+
+    strR = str(R)
+    strR = strR.replace('<var>', '$')
+    strR = strR.replace('</var>', '$')
+    strR = strR.replace('<code>', '`')
+    strR = strR.replace('</code>', '`')
+    strR = strR.replace('<strong>', '**')
+    strR = strR.replace('</strong>', '**')
+    strR = strR.replace('_', '\\_')
+    strR = strR.replace('<li>', '- ')
+    strR = strR.replace('</li>', '')
+    strR = strR.replace('<ul>', '\n')
+    strR = strR.replace('</ul>', '')
+    strR = strR.replace('<ol>', '\n')
+    strR = strR.replace('</ol>', '')
+    strR = strR.replace('<p>', '\n')
+    strR = strR.replace('</p>', '\n')
+    strR = strR.replace('<h3>', '### ')
+    strR = strR.replace('</h3>', '\n ')
+    strR = strR.replace('<section>', '')
+    strR = strR.replace('</section>', '')
+    strR = strR.replace('\n\n', '\n')
+    strR = strR.replace('\n', '\n> ')
+    txt += strR
+
+    txt += "\n"
+    txt += "\n"
+    txt += "\n"
+    txt += "\n---"
+    txt += "\n"
+
+    md.writelines(txt)
+    md.close()
